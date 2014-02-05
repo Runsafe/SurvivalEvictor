d9d39c7a51bb10901a9a37f67d530d07e35b500a
Kruithne <kruithne@gmail.com>
2014-02-05 03:53:19 +0000

    Changed to use IPlayerTeleport over IPlayerChangedWorldEvent

--- src/no/runsafe/survivalevictor/PlayerMonitor.java
+++ src/no/runsafe/survivalevictor/PlayerMonitor.java
@@ -1,12 +1,12 @@
 package no.runsafe.survivalevictor;
 
+import no.runsafe.framework.api.ILocation;
 import no.runsafe.framework.api.IWorld;
-import no.runsafe.framework.api.event.player.IPlayerChangedWorldEvent;
+import no.runsafe.framework.api.event.player.IPlayerTeleport;
 import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.api.player.IPlayer;
-import no.runsafe.framework.minecraft.event.player.RunsafePlayerChangedWorldEvent;
 
-public class PlayerMonitor implements IPlayerChangedWorldEvent
+public class PlayerMonitor implements IPlayerTeleport
 {
 	public PlayerMonitor(IConsole console)
 	{
@@ -14,27 +14,28 @@ public class PlayerMonitor implements IPlayerChangedWorldEvent
 	}
 
 	@Override
-	public void OnPlayerChangedWorld(RunsafePlayerChangedWorldEvent event)
+	public boolean OnPlayerTeleport(IPlayer player, ILocation from, ILocation to)
 	{
-		IPlayer player = event.getPlayer();
-		IWorld world = player.getWorld();
+		IWorld world = to.getWorld();
 
 		if (world == null)
 		{
 			console.logInformation("Player %s is going to a NULL world!", player.getName());
-			return;
+			return true;
 		}
 
 		console.logInformation("Player %s is going to world: %s", player.getName(), world.getName());
 
-		IWorld sourceWorld = event.getSourceWorld();
+		IWorld sourceWorld = from.getWorld();
 		if (sourceWorld == null)
 		{
 			console.logInformation("Player %s is coming from a NULL world", player.getName());
-			return;
+			return true;
 		}
 
 		console.logInformation("Player %s is coming from world: %s", player.getName(), world.getName());
+
+		return true;
 	}
 
 	private final IConsole console;
